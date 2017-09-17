de542337f2a9f176d317b83471e60fc91b1a3bcb
si1kdd <silkdd.davsullivan@zoho.com>
2017-09-18 00:20:41 +0800

    Upload a script to do the partial upgrade on arch linux

---
+++ arch_partial_upgrade.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+# @ Function
+#       Use 'pacman' to do the partial upgrade,
+#       Yes, do this dangerous staff if you don't wanna to do the full system upgrade.
+#
+# @ Usage:
+#       ./arch_partial_upgrade.sh THE_PACKAGES_YOU_WANNA_UPGRADE
+#
+# @ Author:
+#       si1kdd
+#
+# @ Status:
+#       Unfinished ...
+
+set -e
+
+function msg_help() {
+        echo "[*] Usage: ./$0 PACKAGE_NAME"
+        exit -1
+}
+
+function check_upgrade() {
+        upgrade=$(pacman -Qu | wc -l)
+        echo "[+] Now Total $upgrade packages could being upgrade."
+        exit 1
+}
+
+function check_super_user() {
+        if [ "$EUID" -ne 0 ]; then
+                echo "[!] Please run as root or use 'sudo' command ..."
+                echo "          This scripts need privilege. (And only tested on CentOS and Arch Linux)"
+                exit -1
+        fi
+}
+
+if [[ -z $1 ]]; then
+        msg_help
+        exit -1
+fi
+
+exec_str='sudo pacman -S '
+packages=($(pacman -Ss $1 | grep installed: | cut -d/ -f2 | awk '{print $1}'))
+
+echo "[+] Total ${#packages[@]} packages would be upgrade:"
+printf "\t"
+for i in ${packages[@]}; do
+        printf "${i} "
+        exec_str=$exec_str' '${i}
+done
+
+echo
+echo
+
+check_super_user
+# echo $exec_str
+eval $exec_str
+
+echo "[*] Done!"
+check_upgrade
+
+# TODO: may add new features.
